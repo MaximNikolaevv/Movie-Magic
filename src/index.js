b392d248a221629780aa2b2c6f82509fb0817b66
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2025-03-03 22:24:00 +0200

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import { express } from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", req, res => {
+app.get("/", (req, res) => {
 
     res.send("It Works");
 
 
 })
-
-
-
 
 app.listen(5000, () => console.log("Server is listening on http://localhost:5000"));

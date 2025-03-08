f973d9eac24af3966f04fd5a0af60b77d6b05cba
MaximNikolaevv <146669687+MaximNikolaevv@users.noreply.github.com>
2025-03-08 23:29:49 +0200

    Type home with handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import handlebars from "express-handlebars";
 
 const app = express();
 
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs')
+app.set('views', './src/views');
+
+
 app.get("/", (req, res) => {
 
-    res.send("It Works");
+    res.render('home', { layout: false });
 
 
 })
